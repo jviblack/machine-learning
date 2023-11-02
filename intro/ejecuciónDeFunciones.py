f099a9eca9ad2af6214c8b44fe4a44dc476f2f9f
jviblack <javie.couto.dominguez@gmail.com>
2023-11-02 22:19:18 +0100

    commit

---
+++ intro/ejecuciónDeFunciones.py
@@ -0,0 +1,18 @@
+from funcionesExternas import *
+
+funcion1()
+
+q = input('Introduce un dato: ')
+w = input('Introduce otro dato: ')
+
+print(funcion2(q, w))
+
+
+#import funcionesExternas as F
+
+#F.funcion1()
+
+#q = input('Introduce un dato: ')
+#w = input('Introduce otro dato: ')
+
+#print(F.funcion2(q, w))
